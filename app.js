cde55c67ca3d5aa37fae5a90f8f143d5220a5c78
Daniel Boufford <danielboufford@Daniels-MacBook-Pro.local>
2017-06-12 18:31:28 -0400

    1st commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req,res) => {
+	res.send("welcome, friend");
+})
+
+
+
+app.listen(PORT, () => {
+	console.log("listening")}
+	);
