7f703b38b916507a91b73cfac8632f5c79609c4b
Nancy Velasquez <nevelasquez113@gmail.com>
2017-06-13 11:24:29 -0400

    Twitter.js In Progress

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {
+	autoescape: true,
+	noCache: true
+}); // point nunjucks to the proper directory for templates
+
+var locals = {
+    title: 'Favorite Characters',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.get('/', function(req, res){
+	res.render('index.html', locals);
+});
 
 app.use(function (req, res, next){
 	console.log(req.method, req.originalUrl);
 	next();
-})
+});
 
 app.post('/', function(req, res) {
 	res.send(post.method, 'Request to homepage');
 	next();
 });
 
-// app.use('/', (req, res, next) => {
-//   const user = req.query.user;
-//   req.user = user;
-//   next();
-// })
 //
-// app.use('/', (req, res, next) => {
-//   console.log('current user', req.user);
-//   next();
-// })
-
-app.get('/', (req,res) => {
-	res.send("welcome, friend");
-});
+// app.get('/', (req,res) => {
+// 	res.send("welcome, friend");
+// });
 
 app.listen(PORT, () => {
 	console.log("listening")}
