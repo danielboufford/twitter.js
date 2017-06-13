342e432d81af2296e24c1f102e1843dee7f829fb
Nancy Velasquez <nevelasquez113@gmail.com>
2017-06-13 10:40:15 -0400

    Twitter.js In Progress

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const app = express();
 const PORT = 3000;
 
 
-app.get('/', (req,res) => {
-	res.send("welcome, friend");
+app.use(function (req, res, next){
+	console.log(req.method, req.originalUrl);
+	next();
 })
 
+app.post('/', function(req, res) {
+	res.send(post.method, 'Request to homepage');
+	next();
+});
 
+// app.use('/', (req, res, next) => {
+//   const user = req.query.user;
+//   req.user = user;
+//   next();
+// })
+//
+// app.use('/', (req, res, next) => {
+//   console.log('current user', req.user);
+//   next();
+// })
+
+app.get('/', (req,res) => {
+	res.send("welcome, friend");
+});
 
 app.listen(PORT, () => {
 	console.log("listening")}
-	);
+);
